8731545b258572ba2a1367ed5eccdc7a282c3da9
Redwan-Shawkat <redwanshawkat@gmail.com>
2023-12-26 09:30:28 +0600

    redwan

---
+++ style.js
@@ -0,0 +1,6 @@
+let cm_value = Number(prompt("Enter the Centimeter Value: "));
+// let num2 = Number(prompt("Enter 2nd Number: "));
+
+let m_value = cm_value * 100;
+
+alert("cm to m : " + m_value);
