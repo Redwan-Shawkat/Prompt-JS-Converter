271de31efbdd80e3d8116387c91f450c02e12856
Redwan-Shawkat <redwanshawkat@gmail.com>
2023-12-26 09:39:23 +0600

    redwan

---
+++ style.js
@@ -0,0 +1,8 @@
+let tScore = Number(prompt("Enter the Target Score: "));
+let cScore = Number(prompt("Enter the Current Score: "));
+let rOver = Number(prompt("Enter the Remaining Over: "));
+
+
+let rrRate =  (tScore-cScore)/rOver
+
+alert("Required Run Rate : " + rrRate);
